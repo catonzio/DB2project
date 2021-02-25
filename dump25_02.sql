b89b092ee647c30ee4476fcb3ff1012be101d881
chimoser <chiaravmoser@gmail.com>
2021-02-25 18:42:10 +0100

    Update dump25_02.sql

--- dump25_02.sql
+++ dump25_02.sql
@@ -47,7 +47,7 @@ UNLOCK TABLES;
 -- Table structure for table `dirtywords`
 --
 
-DROP TABLE IF EXISTS `dirtywords`;
+DROP TABLE IF EXISTS `dirtyword`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dirtywords` (
